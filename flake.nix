c2df9f49d93b79b3d32cf17842ebe4d2b1c3d150
Simone Camito <zibo.camito@gmail.com>
2024-08-06 15:00:16 +0200

    add flake

--- flake.nix
+++ flake.nix
@@ -24,44 +24,26 @@
         with pkgs;
         {
           devShells.default = mkShell {
-            buildInputs = [ rust-bin.stable.latest.default ];
+            buildInputs = [
+              rust-bin.stable.latest.default
+              rustPlatform.bindgenHook
+              pkg-config
+              libxkbcommon
+              libGL
+              pipewire
+              libpulseaudio
+              wayland
+              vulkan-loader
+            ];
+
+            LD_LIBRARY_PATH = lib.makeLibraryPath [
+              libpulseaudio
+              wayland
+              mesa.drivers
+              vulkan-loader
+              libGL
+            ];
           };
         }
       );
 }
-#
-# {
-#   description = "Barely customizable Wayland status bar for Hyprland compositor.";
-#
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#     rust-overlay.url = "github:oxalica/rust-overlay";
-#   };
-#
-#   outputs = {
-#     nixpkgs,
-#     rust-overlay,
-#     ...
-#   }: let
-#     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-#     forEachSystem = nixpkgs.lib.genAttrs systems;
-#   in {
-#     devShells = forEachSystem (
-#       system: let
-#         pkgs = nixpkgs.legacyPackages.${system};
-#         rustPkgs = rust-overlay.packages.${system};
-#       in rec {
-#         default = ashell;
-#         ashell = pkgs.mkShell {
-#           buildInputs = with pkgs; [
-#             pkg-config
-#
-#             (rustPkgs.rust.override {
-#               extensions = ["rust-src"];
-#             })
-#           ];
-#         };
-#       }
-#     );
-#   };
-# }
