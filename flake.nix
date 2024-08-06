cc7d7fbd2452fb007879c58be505a19d52917756
Simone Camito <zibo.camito@gmail.com>
2024-08-06 16:49:26 +0200

    update

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Barely customizable Wayland status bar for Hyprland compositor.";
 
   inputs = {
+    naersk.url = "github:nmattia/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
@@ -12,17 +13,46 @@
       };
     };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, naersk, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           overlays = [ (import rust-overlay) ];
+          naersk-lib = pkgs.callPackage naersk { };
           pkgs = import nixpkgs {
             inherit system overlays;
           };
+          libPath = with pkgs; lib.makeLibraryPath [
+            libpulseaudio
+            wayland
+            mesa.drivers
+            vulkan-loader
+            libGL
+          ];
+
         in
         with pkgs;
         {
+          # `nix build`
+          defaultPackage = naersk-lib.buildPackage {
+            pname = "ashell";
+            src = ./.;
+            doCheck = true;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            # buildInputs = with pkgs; [
+            #   xorg.libxcb
+            # ];
+            postInstall = ''
+              wrapProgram "$out/bin/ashell" --prefix LD_LIBRARY_PATH : "${libPath}"
+            '';
+          };
+
+          # `nix run`
+          defaultApp = flake-utils.lib.mkApp {
+            drv = self.defaultPackage."${system}";
+          };
+
+          # `nix develop`
           devShells.default = mkShell {
             buildInputs = [
               rust-bin.stable.latest.default
@@ -36,13 +66,7 @@
               vulkan-loader
             ];
 
-            LD_LIBRARY_PATH = lib.makeLibraryPath [
-              libpulseaudio
-              wayland
-              mesa.drivers
-              vulkan-loader
-              libGL
-            ];
+            LD_LIBRARY_PATH = libPath;
           };
         }
       );
