b155f1ec5b020223cfa10ddbfd657f0c14140998
Simone Camito <zibo.camito@gmail.com>
2024-10-23 09:40:19 +0200

    fix log

--- src/outputs.rs
+++ src/outputs.rs
@@ -28,15 +28,14 @@ impl Outputs {
         output_info: Option<OutputInfo>,
         wl_output: WlOutput,
     ) -> Task<Message> {
-        debug!("request_outputs: {:?}", request_outputs);
         let target = request_outputs.iter().any(|output| {
             Some(output.as_str()) == output_info.as_ref().and_then(|info| info.name.as_deref())
         });
-        debug!("target: {:?}", target);
-        let id = Id::unique();
 
         if self.0.is_empty() {
-            let cmd = get_layer_surface(SctkLayerSurfaceSettings {
+            debug!("No outputs, creating a new layer surface. Is a fallback surface {}", target);
+            let id = Id::unique();
+            let task = get_layer_surface(SctkLayerSurfaceSettings {
                 id,
                 size: Some((None, Some(HEIGHT))),
                 layer: Layer::Top,
@@ -65,9 +64,11 @@ impl Outputs {
 
             self.0.push((id, target.then_some(wl_output)));
 
-            cmd
+            task
         } else if target {
-            let create_cmd = get_layer_surface(SctkLayerSurfaceSettings {
+            debug!("Found target output, creating a new layer surface");
+            let id = Id::unique();
+            let create_task = get_layer_surface(SctkLayerSurfaceSettings {
                 id,
                 size: Some((None, Some(HEIGHT))),
                 pointer_interactivity: true,
@@ -85,12 +86,13 @@ impl Outputs {
             self.0.push((id, Some(wl_output)));
 
             if let Some(index) = self.0.iter().position(|(_, wl_output)| wl_output.is_none()) {
+                debug!("Found fallback output, removing it");
                 let (id, _) = self.0.swap_remove(index);
-                let destroy_cmd = destroy_layer_surface(id);
+                let destroy_task = destroy_layer_surface(id);
 
-                Task::batch(vec![create_cmd, destroy_cmd])
+                Task::batch(vec![create_task, destroy_task])
             } else {
-                create_cmd
+                create_task
             }
         } else {
             Task::none()
@@ -107,13 +109,15 @@ impl Outputs {
             .iter()
             .position(|(_, output)| output.as_ref() == Some(&wl_output))
         {
+            debug!("Removing layer surface for output");
             let (id, _) = self.0.swap_remove(to_remove);
 
-            let destroy_cmd = destroy_layer_surface(id);
+            let destroy_task = destroy_layer_surface(id);
 
             if self.0.is_empty() {
+                debug!("No outputs left, creating a fallback layer surface");
                 let id = Id::unique();
-                let create_cmd = get_layer_surface(SctkLayerSurfaceSettings {
+                let create_task = get_layer_surface(SctkLayerSurfaceSettings {
                     id,
                     size: Some((None, Some(HEIGHT))),
                     pointer_interactivity: true,
@@ -130,9 +134,9 @@ impl Outputs {
 
                 self.0.push((id, None));
 
-                Task::batch(vec![destroy_cmd, create_cmd])
+                Task::batch(vec![destroy_task, create_task])
             } else {
-                destroy_cmd
+                destroy_task
             }
         } else {
             Task::none()
