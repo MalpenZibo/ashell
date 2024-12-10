3a63c5604319e8806cfe791a62f23c2b2279f2f2
Simone Camito <zibo.camito@gmail.com>
2024-12-11 00:00:26 +0100

    fix clippy

--- src/utils/launcher.rs
+++ src/utils/launcher.rs
@@ -2,50 +2,55 @@ use std::process::Command;
 
 pub fn execute_command(command: String) {
     tokio::spawn(async move {
-        Command::new("bash")
+        let _ = Command::new("bash")
             .arg("-c")
             .arg(&command)
             .spawn()
-            .unwrap_or_else(|_| panic!("Failed to execute command {}", &command));
+            .unwrap_or_else(|_| panic!("Failed to execute command {}", &command))
+            .wait();
     });
 }
 
 pub fn suspend() {
     tokio::spawn(async move {
-        Command::new("bash")
+        let _ = Command::new("bash")
             .arg("-c")
             .arg("systemctl suspend")
             .spawn()
-            .expect("Failed to execute command.");
+            .expect("Failed to execute command.")
+            .wait();
     });
 }
 
 pub fn shutdown() {
     tokio::spawn(async move {
-        Command::new("bash")
+        let _ = Command::new("bash")
             .arg("-c")
             .arg("shutdown now")
             .spawn()
-            .expect("Failed to execute command.");
+            .expect("Failed to execute command.")
+            .wait();
     });
 }
 
 pub fn reboot() {
     tokio::spawn(async move {
-        Command::new("bash")
+        let _ = Command::new("bash")
             .arg("-c")
             .arg("systemctl reboot")
             .spawn()
-            .expect("Failed to execute command.");
+            .expect("Failed to execute command.")
+            .wait();
     });
 }
 
 pub fn logout() {
     tokio::spawn(async move {
-        Command::new("bash")
+        let _ = Command::new("bash")
             .arg("-c")
             .arg("loginctl kill-user $(whoami)")
             .spawn()
-            .expect("Failed to execute command.");
+            .expect("Failed to execute command.")
+            .wait();
     });
 }
