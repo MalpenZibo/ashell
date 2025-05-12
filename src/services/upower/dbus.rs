4f9e42b929f1b93e664521670bb76014010e0170
Simone Camito <zibo.camito@gmail.com>
2025-05-12 11:55:53 +0200

    handle multiple battery

--- src/services/upower/dbus.rs
+++ src/services/upower/dbus.rs
@@ -14,6 +14,88 @@ impl<'a> Deref for UPowerDbus<'a> {
     }
 }
 
+pub struct Battery(Vec<DeviceProxy<'static>>);
+
+impl Battery {
+    pub async fn state(&self) -> i32 {
+        let mut charging = false;
+        let mut discharging = false;
+        let mut full = false;
+
+        for device in &self.0 {
+            if let Ok(state) = device.state().await {
+                match state {
+                    1 => {
+                        charging = true;
+                    }
+                    2 => {
+                        discharging = true;
+                    }
+                    4 => {
+                        full = true;
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        if charging {
+            return 1;
+        } else if discharging {
+            return 2;
+        } else if full {
+            return 4;
+        }
+
+        return 4;
+    }
+
+    pub async fn percentage(&self) -> f64 {
+        let mut percentage = 0.0;
+        let mut count = 0;
+
+        for device in &self.0 {
+            if let Ok(p) = device.percentage().await {
+                percentage += p;
+                count += 1;
+            }
+        }
+
+        percentage / count as f64
+    }
+
+    pub async fn time_to_empty(&self) -> i64 {
+        let mut time = 0;
+
+        for device in &self.0 {
+            if let Ok(t) = device.time_to_empty().await {
+                time += t;
+            }
+        }
+
+        time
+    }
+
+    pub async fn time_to_full(&self) -> i64 {
+        let mut time = 0;
+
+        for device in &self.0 {
+            if let Ok(t) = device.time_to_full().await {
+                time += t;
+            }
+        }
+
+        time
+    }
+
+    pub fn get_devices_path(self) -> Vec<ObjectPath<'static>> {
+        self.0
+            .into_iter()
+            .map(|device| device.inner().path().to_owned())
+            .collect()
+    }
+}
+
 impl UPowerDbus<'_> {
     pub async fn new(conn: &zbus::Connection) -> anyhow::Result<Self> {
         let nm = UPowerProxy::new(conn).await?;
@@ -21,8 +103,10 @@ impl UPowerDbus<'_> {
         Ok(Self(nm))
     }
 
-    pub async fn get_battery_device(&self) -> anyhow::Result<Option<DeviceProxy>> {
+    pub async fn get_battery_devices(&self) -> anyhow::Result<Option<Battery>> {
         let devices = self.enumerate_devices().await?;
+
+        let mut res = Vec::new();
 
         for device in devices {
             let device = DeviceProxy::builder(self.inner().connection())
@@ -34,11 +118,15 @@ impl UPowerDbus<'_> {
             let power_supply = device.power_supply().await?;
 
             if device_type == 2 && power_supply {
-                return Ok(Some(device));
+                res.push(device);
             }
         }
 
-        Ok(None)
+        if !res.is_empty() {
+            Ok(Some(Battery(res)))
+        } else {
+            Ok(None)
+        }
     }
 
     pub async fn get_device(
