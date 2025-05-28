7660211b3aff0f2473e8ddc5deabb9de384a4529
Ari Seyhun <dev@tqwewe.com>
2025-05-28 20:06:18 +1000

    fix upower system battery reference

--- src/modules/settings/mod.rs
+++ src/modules/settings/mod.rs
@@ -453,7 +453,7 @@ impl Settings {
             let battery_data = self
                 .upower
                 .as_ref()
-                .and_then(|upower| upower.battery)
+                .and_then(|upower| upower.system_battery)
                 .map(|battery| battery.settings_indicator());
             let right_buttons = Row::new()
                 .push_maybe(config.lock_cmd.as_ref().map(|_| {
