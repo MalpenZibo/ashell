13179f551daa9b912f717b5dd8785bc6b0e716cf
Simone Camito <zibo.camito@gmail.com>
2025-10-09 22:03:44 +0200

    update ignore file

--- src/components/icons.rs
+++ src/components/icons.rs
@@ -164,5 +164,5 @@ pub fn icon<'a>(r#type: Icons) -> Text<'a> {
 }
 
 pub fn icon_raw<'a>(s: String) -> Text<'a> {
-    text(s).font(Font::with_name("Ashell Nerd Font"))
+    text(s).font(Font::with_name("Symbols Nerd Font"))
 }
