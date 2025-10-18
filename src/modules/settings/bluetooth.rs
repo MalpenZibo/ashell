64368fdeea7de7179e02b253364308b58cf5cb9c
sudo-Tiz <mathis.worksome@simplelogin.fr>
2025-10-19 01:46:25 +0200

    feat(bluetooth): change indicator icon on connected status

--- src/modules/settings/bluetooth.rs
+++ src/modules/settings/bluetooth.rs
@@ -411,11 +411,15 @@ impl BluetoothSettings {
         &'a self,
         _theme: &'a AshellTheme,
     ) -> Option<Element<'a, Message>> {
-        if let Some(service) = &self.service
-            && service.state == BluetoothState::Active
-            && !service.devices.is_empty()
-        {
-            return Some(icon(StaticIcon::Bluetooth).into());
+        if let Some(service) = &self.service {
+            if service.state == BluetoothState::Active {
+                let connected_count = service.devices.iter().filter(|d| d.connected).count();
+                if connected_count > 0 {
+                    return Some(icon(StaticIcon::BluetoothConnected).into());
+                } else {
+                    return Some(icon(StaticIcon::Bluetooth).into());
+                }
+            }
         }
         None
     }
