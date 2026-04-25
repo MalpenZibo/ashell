75872512fffdff521d72d127834384cd284adea6
Roman Stingler <roman.stingler@gmail.com>
2026-04-25 03:55:33 +0200

    chore: remove redundant temperature conversion function

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,6 @@
 use crate::app::Message;
 use crate::services::upower::PeripheralDeviceKind;
+use crate::utils::celsius_to_fahrenheit;
 use chrono::Locale;
 use hex_color::HexColor;
 use iced::futures::StreamExt;
@@ -271,10 +272,6 @@ impl Default for SystemInfoTemperature {
             format: TemperatureFormat::Celsius,
         }
     }
-}
-
-fn celsius_to_fahrenheit(cel: i32) -> i32 {
-    cel * 9 / 5 + 32
 }
 
 #[derive(Clone, Debug, Deserialize, Default)]
